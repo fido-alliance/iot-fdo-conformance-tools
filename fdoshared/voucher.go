d0ff61e23d23b70b51b63d6f39ca6a58d492ba8d
StefanVrecic <vstefan@hotmail.co.uk>
2022-01-31 20:50:17 +1100

    Pushing changes in case of review

--- fdoshared/voucher.go
+++ fdoshared/voucher.go
@@ -55,33 +55,33 @@ func (h OwnershipVoucher) Validate() (bool, error) {
 	// Verify ProtVersion ??
 
 	// Verify OVDevCertChainHash
-	var concatenatedOVDevCertChain []byte
-	var OVDevCertChain_Certs []byte
-	var cert []X509CertificateBytes
+
+	// “OVDevCertChainHash” = Hash of the concatenation of the contents of each byte string in “OwnershipVoucher.OVDevCertChain”,
+	//  in the presented order. When OVDevCertChain is CBOR null, OVDevCertChainHash is also CBOR null.
+	var OVDevCertChain_Certs []byte             // [cert1, cert2, certn]
+	var concatenatedOVDevCertChain []byte       // [cert1||cert2||certn]
+	var cert []X509CertificateBytes             // cert_n
 	for i, bstrCert := range h.OVDevCertChain { // iterated will return  bstr[cert1] : e.g., X5CHAIN = [ bstr[cert1] ... bstr[certN] ],
 		err = cbor.Unmarshal(bstrCert, &cert) // bstr[cert_n] => cert_n
 		if err != nil {
 			log.Println("Error verifying ownershipVoucher, couldn't decode entry in OVDevCertChain")
 			return false, errors.New("error verifying ownershipVoucher, couldn't decode entry in OVDevCertChain ")
 		}
-		OVDevCertChain_Certs = append(OVDevCertChain_Certs, cert) // push cert to OVDevCertChain_Certs
+		OVDevCertChain_Certs = append(OVDevCertChain_Certs, cert) // append cert to OVDevCertChain_Certs
 	}
-	concatenatedOVDevCertChain = OVDevCertChain_Certs.join()
 
-	// concatenatedOVDevCertChain = append(OVDevCertChain_Certs)
-	// [JS] const OVDevCertChain_Certs = ['Fire', 'Air', 'Water']; (OVDevCertChain_Certs.join('')) => "FireAirWater"
-	calculatedHash, err := GenerateFdoHash(h.concatenatedOVDevCertChain, -16)
+	// [Parallel JS example] : const OVDevCertChain_Certs = ['Fire', 'Air', 'Water']; (OVDevCertChain_Certs.join('')) => "FireAirWater"
+	sep := []byte("")
+	concatenatedOVDevCertChain = bytes.Join(OVDevCertChain_Certs, sep) // [cert1, cert2, certn] => [cert1||cert2||certn]
+
+	calculatedHash, err := GenerateFdoHash(concatenatedOVDevCertChain, -16) // generate hash from [cert1||cert2||certn]
 	if err != nil {
 		log.Println("error verifying ownershipVoucher, couldn't calculate hash for OVDevCertChain. ")
 		return false, errors.New("error verifying ownershipVoucher, couldn't calculate hash for OVDevCertChain")
 	}
-	if bytes.Compare(OVHeaderInst.OVDevCertChainHash, calculatedHash.HashOrHmac[:]) != 0 {
+	if bytes.Compare(OVHeaderInst.OVDevCertChainHash.Hash, calculatedHash.Hash[:]) != 0 { // compare calculatedHash to providedHash
 
 	}
-	// So I need to decode every entry in OVDevCertChain, concatenate them together in a var, then hash that var = OVDevCertChainHash
-
-	// “OVDevCertChainHash” = Hash of the concatenation of the contents of each byte string in “OwnershipVoucher.OVDevCertChain”,
-	//  in the presented order. When OVDevCertChain is CBOR null, OVDevCertChainHash is also CBOR null.
 
 	// Verify OVDevCertChain
 	//
