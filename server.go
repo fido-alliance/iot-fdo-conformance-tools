45a724be7698c36aa1e3fb6a0b63ff59c7570f93
Ackermann Yuriy <ackermann.yuriy@gmail.com>
2022-08-06 23:31:32 +0300

    Modulirised DO

---
+++ server.go
@@ -0,0 +1,29 @@
+package fdodo
+
+import (
+	"net/http"
+
+	"github.com/WebauthnWorks/fdo-do/dbs"
+	"github.com/WebauthnWorks/fdo-do/to2"
+	"github.com/dgraph-io/badger/v3"
+)
+
+func SetupServer(db *badger.DB) {
+	// voucher := Voucher{
+	// 	session: dbs.NewSessionDB(db),
+	// }
+
+	DoTo2 := to2.DoTo2{
+		Session: dbs.NewSessionDB(db),
+		Voucher: dbs.NewVoucherDB(db),
+	}
+
+	// http.HandleFunc("/fdo/voucher", voucher.voucherHandler)
+	// http.HandleFunc("/fdo/register-voucher-sender", voucher.register)
+	http.HandleFunc("/fdo/101/msg/60", DoTo2.HelloDevice60)
+	http.HandleFunc("/fdo/101/msg/62", DoTo2.GetOVNextEntry62)
+	http.HandleFunc("/fdo/101/msg/64", DoTo2.ProveDevice64)
+	http.HandleFunc("/fdo/101/msg/66", DoTo2.DeviceServiceInfoReady66)
+	http.HandleFunc("/fdo/101/msg/68", DoTo2.DeviceServiceInfo68)
+	http.HandleFunc("/fdo/101/msg/70", DoTo2.Done70)
+}
