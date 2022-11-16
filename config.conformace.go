bff37183136008079d42a0ba84ecc39e355695be
Ackermann Yuriy <ackermann.yuriy@gmail.com>
2022-11-16 22:35:36 +0400

    Added modes definition

--- config.conformace.go
+++ config.conformace.go
@@ -6,3 +6,10 @@ const (
 	CFG_RESULTS_API_KEY CONFIG_ENTRY = "results_api_key"
 	CFG_MODE            CONFIG_ENTRY = "mode"
 )
+
+type CONFIG_MODE_TYPE string
+
+const (
+	CFG_MODE_ONPREM CONFIG_MODE_TYPE = "onprem"
+	CFG_MODE_ONLINE CONFIG_MODE_TYPE = "online"
+)
