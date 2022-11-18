3bf3c6df3af0899cb0e92746be4b541389029ac0
Ackermann Yuriy <ackermann.yuriy@gmail.com>
2022-11-19 02:58:39 +0400

    Added reset methods. Updated var names

--- testcom/dbs/listener.db.go
+++ testcom/dbs/listener.db.go
@@ -14,18 +14,18 @@ import (
 )
 
 type ListenerTestDB struct {
-	db            *badger.DB
-	prefix        []byte
-	mapperGuidMap []byte
-	ttl           int
+	db               *badger.DB
+	prefix           []byte
+	mapperGuidPrefix []byte
+	ttl              int
 }
 
 func NewListenerTestDB(db *badger.DB) *ListenerTestDB {
 	return &ListenerTestDB{
-		db:            db,
-		prefix:        []byte("lstdb-"),
-		mapperGuidMap: []byte("lstdb-guid-map-"),
-		ttl:           60 * 60 * 24 * 183, //6months storage
+		db:               db,
+		prefix:           []byte("lstdb-"),
+		mapperGuidPrefix: []byte("lstdb-guid-map-"),
+		ttl:              60 * 60 * 24 * 183, //6months storage
 	}
 }
 
@@ -34,13 +34,13 @@ func (h *ListenerTestDB) getEntryId(entryUuid []byte) []byte {
 }
 
 func (h *ListenerTestDB) getMappingEntryId(guid fdoshared.FdoGuid) []byte {
-	return append(h.mapperGuidMap, guid[:]...)
+	return append(h.mapperGuidPrefix, guid[:]...)
 }
 
 func (h *ListenerTestDB) Save(reqListener listenertestsdeps.RequestListenerInst) error {
 	structBytes, err := cbor.Marshal(reqListener)
 	if err != nil {
-		return errors.New("Failed to marshal listener entry. The error is: " + err.Error())
+		return errors.New("Failed to marshal listener entry." + err.Error())
 	}
 
 	dbtxn := h.db.NewTransaction(true)
@@ -49,17 +49,17 @@ func (h *ListenerTestDB) Save(reqListener listenertestsdeps.RequestListenerInst)
 	entry := badger.NewEntry(h.getEntryId(reqListener.Uuid), structBytes).WithTTL(time.Second * time.Duration(h.ttl))
 	err = dbtxn.SetEntry(entry)
 	if err != nil {
-		return errors.New("Failed creating listener db entry instance. The error is: " + err.Error())
+		return errors.New("Failed creating listener db entry instance." + err.Error())
 	}
 
 	err = dbtxn.Commit()
 	if err != nil {
-		return errors.New("Failed saving listener entry. The error is: " + err.Error())
+		return errors.New("Failed saving listener entry." + err.Error())
 	}
 
 	h.SaveMapping(reqListener.Guid, reqListener.Uuid)
 	if err != nil {
-		return errors.New("Failed saving listener entry guid mapping. The error is: " + err.Error())
+		return errors.New("Failed saving listener entry guid mapping." + err.Error())
 	}
 
 	return nil
@@ -68,7 +68,7 @@ func (h *ListenerTestDB) Save(reqListener listenertestsdeps.RequestListenerInst)
 func (h *ListenerTestDB) Update(reqListener listenertestsdeps.RequestListenerInst) error {
 	structBytes, err := cbor.Marshal(reqListener)
 	if err != nil {
-		return errors.New("Failed to marshal listener entry. The error is: " + err.Error())
+		return errors.New("Failed to marshal listener entry." + err.Error())
 	}
 
 	dbtxn := h.db.NewTransaction(true)
@@ -77,12 +77,12 @@ func (h *ListenerTestDB) Update(reqListener listenertestsdeps.RequestListenerIns
 	entry := badger.NewEntry(h.getEntryId(reqListener.Uuid), structBytes).WithTTL(time.Second * time.Duration(h.ttl))
 	err = dbtxn.SetEntry(entry)
 	if err != nil {
-		return errors.New("Failed creating rvte db entry instance. The error is: " + err.Error())
+		return errors.New("Failed creating rvte db entry instance." + err.Error())
 	}
 
 	err = dbtxn.Commit()
 	if err != nil {
-		return errors.New("Failed saving rvte entry. The error is: " + err.Error())
+		return errors.New("Failed saving rvte entry." + err.Error())
 	}
 
 	return nil
@@ -96,22 +96,104 @@ func (h *ListenerTestDB) Get(entryUuid []byte) (*listenertestsdeps.RequestListen
 	if err != nil && errors.Is(err, badger.ErrKeyNotFound) {
 		return nil, fmt.Errorf("The rvte entry with id %s does not exist", hex.EncodeToString(h.getEntryId(entryUuid)))
 	} else if err != nil {
-		return nil, errors.New("Failed locating rvte entry. The error is: " + err.Error())
+		return nil, errors.New("Failed locating rvte entry." + err.Error())
 	}
 
 	itemBytes, err := item.ValueCopy(nil)
 	if err != nil {
-		return nil, errors.New("Failed reading rvte entry value. The error is: " + err.Error())
+		return nil, errors.New("Failed reading rvte entry value." + err.Error())
 	}
 
 	var reqListInst listenertestsdeps.RequestListenerInst
 	err = cbor.Unmarshal(itemBytes, &reqListInst)
 	if err != nil {
-		return nil, errors.New("Failed cbor decoding rvte entry value. The error is: " + err.Error())
+		return nil, errors.New("Failed cbor decoding rvte entry value." + err.Error())
 	}
 
 	return &reqListInst, nil
 }
+
+func (h *ListenerTestDB) DeleteMapping(guid fdoshared.FdoGuid) error {
+	dbtxn := h.db.NewTransaction(true)
+	defer dbtxn.Discard()
+
+	err := dbtxn.Delete(h.getMappingEntryId(guid))
+	if err != nil {
+		return errors.New("Failed initialise delete mapping entry." + err.Error())
+	}
+
+	err = dbtxn.Commit()
+	if err != nil {
+		return errors.New("Failed to delete guid mapping." + err.Error())
+	}
+
+	return nil
+}
+
+func (h *ListenerTestDB) DeleteEntry(entryUuid []byte) error {
+	dbtxn := h.db.NewTransaction(true)
+	defer dbtxn.Discard()
+
+	err := dbtxn.Delete(h.getEntryId(entryUuid))
+	if err != nil {
+		return errors.New("Failed initialise delete listener entry." + err.Error())
+	}
+
+	err = dbtxn.Commit()
+	if err != nil {
+		return errors.New("Failed to delete listener entry." + err.Error())
+	}
+
+	return nil
+}
+
+func (h *ListenerTestDB) Delete(entryUuid []byte) error {
+	entry, err := h.Get(entryUuid)
+	if err != nil {
+		return errors.New("Failed initialise delete listener entry. " + err.Error())
+	}
+
+	err = h.DeleteMapping(entry.Guid)
+	if err != nil {
+		return err
+	}
+
+	err = h.DeleteEntry(entryUuid)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (h *ListenerTestDB) ResetDB() error {
+	dbtxn := h.db.NewTransaction(true)
+	defer dbtxn.Discard()
+
+	iterTxn := dbtxn.NewIterator(badger.IteratorOptions{})
+	defer iterTxn.Close()
+	for iterTxn.Rewind(); iterTxn.ValidForPrefix(h.prefix) || iterTxn.ValidForPrefix(h.mapperGuidPrefix); iterTxn.Next() {
+		item := iterTxn.Item()
+		k := item.Key()
+
+		log.Println("Deleting... " + hex.EncodeToString(k))
+
+		err := dbtxn.Delete(k)
+		if err != nil {
+			log.Println("Error creating delete mapping... " + hex.EncodeToString(k))
+		}
+
+		err = dbtxn.Commit()
+		if err != nil {
+			log.Println("Failed to commit delete mapping... " + hex.EncodeToString(k))
+		}
+
+	}
+
+	return nil
+}
+
+/* ---- Test mgmt menthods ----- */
 
 func (h *ListenerTestDB) GetMany(entriesIds [][]byte) (*[]listenertestsdeps.RequestListenerInst, error) {
 	var listenerEntries []listenertestsdeps.RequestListenerInst
@@ -176,12 +258,12 @@ func (h *ListenerTestDB) SaveMapping(guid fdoshared.FdoGuid, uuid []byte) error 
 	entry := badger.NewEntry(h.getMappingEntryId(guid), uuid)
 	err := dbtxn.SetEntry(entry)
 	if err != nil {
-		return errors.New("Failed creating listener db mapping entry instance. The error is: " + err.Error())
+		return errors.New("Failed creating listener db mapping entry instance." + err.Error())
 	}
 
 	err = dbtxn.Commit()
 	if err != nil {
-		return errors.New("Failed saving listener mapping entry. The error is: " + err.Error())
+		return errors.New("Failed saving listener mapping entry." + err.Error())
 	}
 
 	return nil
@@ -195,12 +277,12 @@ func (h *ListenerTestDB) GetMappingEntry(guid fdoshared.FdoGuid) ([]byte, error)
 	if err != nil && errors.Is(err, badger.ErrKeyNotFound) {
 		return nil, fmt.Errorf("The mapping entry with id %s does not exist", hex.EncodeToString(h.getMappingEntryId(guid)))
 	} else if err != nil {
-		return nil, errors.New("Failed locating mapping entry. The error is: " + err.Error())
+		return nil, errors.New("Failed locating mapping entry." + err.Error())
 	}
 
 	itemBytes, err := item.ValueCopy(nil)
 	if err != nil {
-		return nil, errors.New("Failed reading mapping entry value. The error is: " + err.Error())
+		return nil, errors.New("Failed reading mapping entry value." + err.Error())
 	}
 
 	return itemBytes, nil
