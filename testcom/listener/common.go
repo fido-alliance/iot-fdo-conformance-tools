a15c8ef24dfb4922153ff6d2f1ff8f26c5627f54
Ackermann Yuriy <ackermann.yuriy@gmail.com>
2022-11-24 04:15:55 +0900

    Moved Conf_RespondFDOError to fdoshared

--- testcom/listener/common.go
+++ testcom/listener/common.go
@@ -1,1 +1,22 @@
 package listener
+
+import (
+	"net/http"
+
+	fdoshared "github.com/WebauthnWorks/fdo-shared"
+)
+
+func Conf_RespondFDOError(w http.ResponseWriter, r *http.Request, errorCode fdoshared.FdoErrorCode, prevMsgId fdoshared.FdoCmd, messageStr string, httpStatusCode int, testcomListener *RequestListenerInst, fdoProtocol fdoshared.FdoToProtocol) {
+	if testcomListener != nil {
+		switch fdoProtocol {
+		case fdoshared.To0:
+			testcomListener.To0.PushFail(messageStr)
+		case fdoshared.To1:
+			testcomListener.To1.PushFail(messageStr)
+		case fdoshared.To2:
+			testcomListener.To2.PushFail(messageStr)
+		}
+	}
+
+	fdoshared.RespondFDOError(w, r, errorCode, prevMsgId, messageStr, httpStatusCode)
+}
