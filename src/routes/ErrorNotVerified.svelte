e380823d935819775d56f036e2f7e4b73797df98
Ackermann Yuriy <ackermann.yuriy@gmail.com>
2023-01-15 12:09:49 +0100

    :lipstick:

---
+++ src/routes/ErrorNotVerified.svelte
@@ -0,0 +1,8 @@
+<section id="first" class="main special">
+    <header class="major">
+        <h2>Error: Your account pending approval.</h2>
+    </header>
+    <ul class="features">
+       <p>Your account pending approval by FIDO Alliance staff. Once your account is approved you will receive confirmation email.</p>
+    </ul>
+</section>
