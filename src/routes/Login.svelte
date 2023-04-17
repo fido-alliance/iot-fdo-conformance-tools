fd074918f3dc4cc4a8e91ace777fe12835001429
Ackermann Yuriy <ackermann.yuriy@gmail.com>
2023-04-17 16:39:44 +0200

    :lipstick:

--- src/routes/Login.svelte
+++ src/routes/Login.svelte
@@ -130,16 +130,14 @@
 
                         <div class="col-6">
                             <ul class="actions">
-                                <li><a href="/#/" style="color:#ffffffcc !important" class="button oauth github" on:click={handleGithubLogin} ><span class="fab fa-github"></span> Login with Github </a></li>
-                                <!-- <li><a href="/#/" dis class="button oauth google"><span class="fab fa-google"></span> Login with Google</a></li> -->
+                                <li><a href="/#/" style="color:#ffffffcc !important" class="button oauth github" on:click={handleGithubLogin} ><span class="fab fa-github"></span>Login with Github </a></li>
                             </ul>
                         </div>
 
 
                         <div class="col-6">
                             <ul class="actions">
-                                <li><a href="/#/" style="color:#000000cc !important" class="button oauth google" on:click={handleGoogleLogin} ><span class="fab fa-google"></span> Login with Google </a></li>
-                                <!-- <li><a href="/#/" dis class="button oauth google"><span class="fab fa-google"></span> Login with Google</a></li> -->
+                                <li><a href="/#/" style="color:#000000cc !important" class="button oauth google" on:click={handleGoogleLogin} ><span class="fab fa-google"></span>Login with Google </a></li>
                             </ul>
                         </div>
                     {/if}
