d5ab8e9b58a3c1d6c23eeb32fe815939e74db84e
Ackermann Yuriy <ackermann.yuriy@gmail.com>
2024-03-11 09:10:27 +1300

    Added missing run status to the UI

--- frontend/src/routes/Device.fdo.svelte
+++ frontend/src/routes/Device.fdo.svelte
@@ -225,8 +225,9 @@
                                 <div class="row">
                                     <div class="col-12 col-12-xsmall">
                                         <input type="radio" id="trun-radio-{run.uuid}" value="{run.uuid}" name="testrun-radio" bind:group={selectedTestRunUuid}>
-                                        <label for="trun-radio-{run.uuid}">{getRunDate(run)} <a href="#" on:click|preventDefault={() => handleRemoveTestRun(run.protocol, run.uuid)}>X</a></label>
-                                    </div>
+                                        <label for="trun-radio-{run.uuid}"><b>{getRunState(run)}</b>: {getRunDate(run)}
+                                            <a href="#" on:click|preventDefault={() => handleRemoveTestRun(run.protocol, run.uuid)}>X</a></label>
+                                        </div>
                                 </div>
                                 {/each}
                             {/if}
